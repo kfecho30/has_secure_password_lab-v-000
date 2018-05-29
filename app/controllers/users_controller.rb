2f393ab33ab27729aa0760704100c78f6ed550ed
Kelsey Fecho <kfecho30@gmail.com>
2018-05-29 14:24:06 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.find_or_create_by(:name => params[:name])
+    user = User.find_by(name: params[:name])
     redirect_to '/users/new'
   end
 
