fc92a584f7ecc59525e269c5cb0196dfc6460fa4
Kelsey Fecho <kfecho30@gmail.com>
2018-05-29 14:22:04 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.find_or_create_by(:name => params[:name])
-    redirect_to '/welcome'
+    redirect_to 'users/new'
   end
 
   def welcome
