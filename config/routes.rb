d8f870d71982687860d8bb43fecf1a50eb7fcc76
Kelsey Fecho <kfecho30@gmail.com>
2018-05-29 14:21:27 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :users
+  get '/welcome' => 'users#welcome'
 
   # Example resource route with options:
   #   resources :products do
